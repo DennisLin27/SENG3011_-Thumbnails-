e48358f213ffa8d7b5eb3513670b614813ca6931
Zixiang Lin <z5314168@ad.unsw.edu.au>
2022-03-18 15:45:36 +1100

    log file back end

---
+++ Phase_1/API_SourceCode/lambda_function.py
@@ -0,0 +1,22 @@
+import re
+from pymongo import MongoClient
+from listeria import listeria_scraper
+from ebola import ebola_scraper
+
+def lambda_handler(event, context):
+    cluster = "mongodb+srv://thumbnails:thumbnails@cluster0.lfkm3.mongodb.net/SENG3011?retryWrites=true&w=majority"
+    client = MongoClient(cluster)
+
+    # Select the database and the cluster 
+    db = client.SENG3011
+    collection = db.SENG3011_collection
+
+    # def clear_db():
+    result = collection.delete_many({})
+
+    # def update_dp():
+    results = []
+    for x in listeria_scraper():
+        result = collection.insert_one(x) 
+    for x in ebola_scraper():
+        result = collection.insert_one(x)
